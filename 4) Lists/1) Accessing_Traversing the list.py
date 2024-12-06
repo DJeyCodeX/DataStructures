228ce71b4db2174ba3b4279e060c04437112fcbc
Divyansh Jain <divyanshjain837@gmail.com>
2024-12-06 17:33:18 +0530

    Add files via upload

---
+++ 4) Lists/1) Accessing_Traversing the list.py
@@ -0,0 +1,8 @@
+shoppingList = ['Milk', 'Cheese', 'Butter']
+
+for i in range(len(shoppingList)):
+    shoppingList[i] = shoppingList[i]+"+"
+    # print(shoppingList[i])
+empty = []
+for i in empty:
+    print("I am empty")
