d38414609df17b7f578ab3bc05f30eed3afadce4
Divyansh Jain <divyanshjain837@gmail.com>
2024-12-06 17:31:10 +0530

    Add files via upload

---
+++ 2) Classes & Pointers/2) Pointers.py
@@ -0,0 +1,50 @@
+num1 = 11
+
+num2 = num1
+
+print("Before num2 value is updated:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+print("\nnum1 points to:", id(num1))
+print("num2 points to:", id(num2)) 
+
+num2 = 22 
+
+print("\nAfter num2 value is updated:")
+print("num1 =", num1)
+print("num2 =", num2) 
+
+print("\nnum1 points to:", id(num1))
+print("num2 points to:", id(num2))
+
+# After udating num2 = 22, num1 won't affect bcz integrers are immutable and doen't change
+
+#####################################
+
+
+dict1 = {
+         'value': 11
+        }
+
+dict2 = dict1 
+
+print("\n\nBefore value is updated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2)
+
+print("\ndict1 points to:", id(dict1))
+print("dict2 points to:", id(dict2)) 
+
+dict2['value'] = 22
+
+print("\nAfter value is updated:")
+print("dict1 =", dict1)
+print("dict2 =", dict2) 
+
+print("\ndict1 points to:", id(dict1))
+print("dict2 points to:", id(dict2))
+
+
+# After udating dict2 = 22, dict1 will also be updated bcz dictionaries are mutable and does change, 
+# Hence, dict1 will also be equal to 22, bcz dict2 = dict1
