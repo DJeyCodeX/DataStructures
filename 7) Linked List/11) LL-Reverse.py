ceef3959789c6d41cc9d387e7352f2880e984f2c
Divyansh Jain <divyanshjain837@gmail.com>
2024-12-06 17:34:30 +0530

    Add files via upload

---
+++ 7) Linked List/11) LL-Reverse.py
@@ -0,0 +1,153 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+            
+
+    def make_empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+        
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        else:
+            pre = self.head
+            temp = self.head
+            while temp.next is not None:
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            self.tail.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return temp.value
+        
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head ## join new_node.next value ith cuurent value of head 
+            self.head = new_node
+        self.length += 1
+        return True
+    
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            self.length -= 1
+            if self.length == 0:
+                self.tail = None
+        return temp.value
+    
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        else:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            return temp
+
+
+    def set(self, index, value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        else:
+            return False
+
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return None
+        elif index == 0:
+            return self.prepend(value)
+        elif index == self.length:
+            return self.append(value)
+        else:
+            new_node = Node(value)
+            temp = self.get(index-1)
+            new_node.next = temp.next
+            temp.next = new_node
+            self.length += 1
+            return True
+
+
+    def remove(self, index):
+        if index < 0 or index > self.length:
+            return None
+        elif index == 0:
+            return self.pop_first()
+        elif index == self.length - 1:
+            return self.pop()
+        else:
+            pre = self.get(index-1)
+            temp = pre.next
+            pre.next = temp.next
+            temp.next = None
+            self.length -= 1
+            return temp
+
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+
+my_linked_list = LinkedList(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+my_linked_list.append(6)
+
+my_linked_list.reverse()
+
+my_linked_list.print_list()
